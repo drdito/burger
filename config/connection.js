ca9a0b9abb20931b4203925429337f1822d32ddc
Daniel Dito <drdito8@gmail.com>
2017-10-25 21:42:20 -0700

    added JAWSDB

--- config/connection.js
+++ config/connection.js
@@ -4,13 +4,18 @@ var mysql = require("mysql");
 
 // Connection to burgers database
 //========================================================
-var connection = mysql.createConnection({
-  port: 3306,
-  host: "localhost",
-  user: "root",
-  password: "Dantheman065$",
-  database: "burgers_db"
-});
+var connection;
+
+if (process.env.JAWSDB_URL) {
+  connection=mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'Dantheman065$',
+    database: 'burgers_db'
+  });
+};
 
 // Connect to the database
 //=========================================================
